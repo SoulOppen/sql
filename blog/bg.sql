cb383e13e37dd853ff3f7ed8ce96ac7eb0d9aabd
oppen <arieloppenheimer@outlook.cl>
2022-03-31 00:18:32 -0300

    final Top

--- blog/bg.sql
+++ blog/bg.sql
@@ -10,4 +10,10 @@ CREATE TABLE comentarios (id INT,usuarios_id INT,post_id INT,titulo VARCHAR(30),
 \COPY usuarios (id,email) FROM  'C:\Users\AOppen\Documents\GitHub\Trabajo\Curso\sql\blog\usuarios.csv' csv header;
 \COPY post (id,usuarios_id,titulo,fecha) FROM  'C:\Users\AOppen\Documents\GitHub\Trabajo\Curso\sql\blog\post.csv' csv header;
 \COPY comentarios (id,usuarios_id,post_id,titulo,fecha) FROM  'C:\Users\AOppen\Documents\GitHub\Trabajo\Curso\sql\blog\comentarios.csv' csv header;
- 
+  --Busquedas--
+  SELECT id,email,post_id,titulo,fecha FROM usuarios INNER JOIN post ON usuarios.id=usuarios_id WHERE id=5
+  SELECT usuarios.id,email,comentarios.id,titulo,fecha FROM usuarios INNER JOIN comentarios on usuarios.id=usuarios_id WHERE email!='usuario06@hotmail.com';
+  SELECT usuarios.id FROM usuarios WHERE usuarios.id NOT IN (SELECT usuarios.id FROM usuarios INNER JOIN post ON usuarios.id=post.usuarios_id) ORDER BY id ASC;
+  SELECT * FROM post FULL OUTER JOIN comentarios ON post.id=comentarios.post_id ORDER BY post.id ASC;
+  SELECT usuarios_id FROM comentarios WHERE fecha BETWEEN '2020-6-1' AND '2020-6-30';
+----
